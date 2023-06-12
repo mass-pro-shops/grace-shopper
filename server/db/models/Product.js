5caca8452bb82dd7c70bb68c08731487ef7b089f
kaichong lin <coco.keven@gmail.com>
2023-06-12 15:14:08 -0500

    models created for user and product,assosiation asigned

---
+++ server/db/models/Product.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product',{
+    name:{
+        type:Sequelize.STRING,
+        allowNull: false
+    },
+    price:{
+        type:Sequelize.FLOAT,
+        allowNull:false,  
+    },
+    description:{
+        type:Sequelize.TEXT,
+        allowNull:false,
+    },
+    image:{
+        type:Sequelize.STRING,
+        allowNull:false,
+        defaultValue:'public/images/productImage.jpeg'
+    },
+    rating:{
+        type:Sequelize.FLOAT,
+        allowNull:true,
+        validate:{
+            min:0,
+            max:5
+        }
+    },
+    quantity:{
+        type:Sequelize.INTEGER,
+        allowNull:false
+    }
+})
+
+module.exports = Product

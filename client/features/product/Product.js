4add661ca6a9dcc38e20a221363878d5b103a9aa
Angelgonzalez98 <gonzalez.angel8991@gmail.com>
2023-06-13 12:26:04 -0700

    single product and route

---
+++ client/features/product/Product.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const Product = () => {
+  const { productId } = useParams();
+
+  const product = {
+    id: productId,
+    name: 'Example Product',
+    description: 'Make a cool description later',
+    price: 9.99,
+    // Other product details
+  };
+
+  return (
+    <div className='singleProduct'>
+      <h1>Product Name and Details??</h1>
+      <p>Product ID: {product.id}</p>
+      <h2>{product.name}</h2>
+      <p>{product.description}</p>
+      <p>Price: ${product.price}</p>
+      {/* Render other product details */}
+    </div>
+  );
+};
+//to do list
+//get example images
+//fix product id
+//rating
+//buy button
+// import navbar and footer later
+export default Product

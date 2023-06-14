b6d7c21673d864299ce336eae1e964b64de86ece
skyleramorton <skyleramorton@gmail.com>
2023-06-14 15:19:19 -0500

    finished main page component and styling

--- client/features/allProducts/AllProducts.jsx
+++ client/features/allProducts/AllProducts.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchAllProducts, getAllProducts } from './allProducts';
 import { Link } from 'react-router-dom';
+import { Categories } from '../index';
 
 const AllProducts = () => {
     const dispatch = useDispatch();
@@ -12,23 +13,34 @@ const AllProducts = () => {
     }, []);
 
     return (
-        <div>
-            <div>
-                <h1>Products</h1>
-            </div>
-            <div>
-                {productsList && productsList.length ? (
-                    productsList.map((product) => (
-                        <div key={product.id}>
-                            <h3>{product.name}</h3>
-                            <Link to={`./${product.id}`}>
-                                <img src={product.image} alt={product.name} />
-                            </Link>
-                        </div>
-                    ))
-                ) : (
-                    <h3>no data</h3>
-                )}
+        <div className="main-section">
+            <Categories />
+            <div className="products-section">
+                <div className="products-header">
+                    <h1>Products</h1>
+                </div>
+                <div className="products-container">
+                    {productsList && productsList.length ? (
+                        productsList.map((product) => (
+                            <div key={product.id} className="product-card">
+                                <h3>{product.name}</h3>
+                                <Link to={`./${product.id}`}>
+                                    <img
+                                        src={product.image}
+                                        alt={product.name}
+                                    />
+                                </Link>
+                                <button className="buy-button">
+                                    <span className="material-symbols-outlined">
+                                        add_shopping_cart
+                                    </span>
+                                </button>
+                            </div>
+                        ))
+                    ) : (
+                        <h3>no data</h3>
+                    )}
+                </div>
             </div>
         </div>
     );
